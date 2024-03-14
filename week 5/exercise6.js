fc16ef1cad134b008c4ecc2221661390a65baa42
danlarteygh <danlarteygh@gmail.com>
2024-03-14 23:25:03 +0000

    after button is clicked, timer is used to reduce opacity every second

---
+++ week 5/exercise6.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded",myLoadFunction);
+
+function myLoadFunction(){
+    var element = document.getElementById("circle");
+    element.style.opacity = 1;
+    element.addEventListener("click", clickFunction);
+
+}
+
+function clickFunction(){
+    setInterval(reduceOpacity, 1000);
+    
+}
+
+function reduceOpacity(){
+    var element = document.getElementById("circle");
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity - 0.1;
+
+}
