bcddd3ada44c4c9a6c9cc849232b23facc27a36e
danlarteygh <danlarteygh@gmail.com>
2024-03-14 23:17:53 +0000

    reduce button opacity when button is clicked

---
+++ week 5/exercise5.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded",myLoadFunction);
+
+function myLoadFunction(){
+    var element = document.getElementById("circle");
+    element.style.opacity = 0.5;
+    element.addEventListener("click", clickFunction);
+
+}
+
+function clickFunction(){
+    var element = document.getElementById("circle");
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity - 0.1;
+}
