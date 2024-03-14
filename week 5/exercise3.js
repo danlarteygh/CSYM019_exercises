b5f444f900efe360f15837230033c0534ec15d97
danlarteygh <danlarteygh@gmail.com>
2024-03-14 23:11:56 +0000

    change button opacity

---
+++ week 5/exercise3.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded",myLoadFunction);
+
+function myLoadFunction(){
+    var element = document.getElementById("circle");
+    element.style.opacity = 0.5;
+}
