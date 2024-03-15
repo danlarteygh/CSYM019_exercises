ad03eccf030b89e8ec76fca226f606c9d226eb64
danlarteygh <danlarteygh@gmail.com>
2024-03-15 11:55:43 +0000

    button moves in direction of whatever arrow key is pressed each time

--- week 5/exercise9.js
+++ week 5/exercise9.js
@@ -4,7 +4,13 @@ function myKeyDown(event){
     //console.log(event.keyCode);
     var element = document.getElementById("circle");
     var positionLeft = element.offsetLeft;
+    var positionTop = element.offsetTop;
     var key = event.keyCode;
-    if(key == 37){element.style.left = parseInt(positionLeft) - 10 + "px" ;}
+
+    if(key == 37){element.style.left = parseInt(positionLeft) - 10 + "px";}
+    else if (key == 39){element.style.left = parseInt(positionLeft) + 10 + "px";}
+    else if (key == 38){element.style.top = parseInt(positionTop) - 10 + "px";}
+    else if (key == 40){element.style.top = parseInt(positionTop) + 10 + "px";}
+
 
 }
