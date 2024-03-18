2ab43146f6bedefa50d45bd5723cf567c95bce01
danlarteygh <danlarteygh@gmail.com>
2024-03-18 21:48:50 +0000

    button slides off screen in direction of arrow key pressed

--- week 5/exercise9.js
+++ week 5/exercise9.js
@@ -1,16 +1,43 @@
-document.addEventListener("keydown", myKeyDown);
+document.addEventListener('DOMContentLoaded', myLoadEvent);
 
-function myKeyDown(event){
+function myLoadEvent() {
+    document.addEventListener("keydown", myKeyDown);
+}
+
+
+function myKeyDown(event) {
     //console.log(event.keyCode);
+
+
+    var key = event.keyCode;
+
+    if (key == 37) { setInterval(leftKey, 10); }
+    else if (key == 39) { setInterval(rightKey, 10); }
+    else if (key == 38) { setInterval(upKey, 10); }
+    else if (key == 40) { setInterval(downKey, 10); }
+}
+
+function leftKey() {
     var element = document.getElementById("circle");
     var positionLeft = element.offsetLeft;
     var positionTop = element.offsetTop;
-    var key = event.keyCode;
-
-    if(key == 37){element.style.left = parseInt(positionLeft) - 10 + "px";}
-    else if (key == 39){element.style.left = parseInt(positionLeft) + 10 + "px";}
-    else if (key == 38){element.style.top = parseInt(positionTop) - 10 + "px";}
-    else if (key == 40){element.style.top = parseInt(positionTop) + 10 + "px";}
-
-
+    element.style.left = parseInt(positionLeft) - 1 + "px";
+}
+function rightKey() {
+    var element = document.getElementById("circle");
+    var positionLeft = element.offsetLeft;
+    var positionTop = element.offsetTop;
+    element.style.left = parseInt(positionLeft) + 1 + "px";
+}
+function upKey() {
+    var element = document.getElementById("circle");
+    var positionLeft = element.offsetLeft;
+    var positionTop = element.offsetTop;
+    element.style.top = parseInt(positionTop) - 1 + "px";
+}
+function downKey() {
+    var element = document.getElementById("circle");
+    var positionLeft = element.offsetLeft;
+    var positionTop = element.offsetTop;
+    element.style.top = parseInt(positionTop) + 1 + "px";
 }
